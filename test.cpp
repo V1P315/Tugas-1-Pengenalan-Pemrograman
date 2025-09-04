9b2b6433e9635e1530d859beb7962f9cf3e52346
V1P315 <abderios32@gmail.com>
2025-09-04 19:40:29 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string judul;
+    
+    cout << "KISAH JULIANA DAN ANTONIO\n";
+    cout << "KING ANTONIO MENCOBA MENCARI TAHU KEBERADAAN PUTRI JULIANA\n";
+    return 0;
+}
